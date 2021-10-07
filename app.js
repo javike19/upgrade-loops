d8805a420e3bdb9c76952d3b5fd18c22e9073a10
unknown <javimorenomontejo@gmail.com>
2021-10-07 20:24:33 +0200

    Iteration#2

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ for (let i = 0; i < products.length; i++) {
         console.log(products[i]);
     }
 }
+
+//Iteración #2
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+	{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+	{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+	{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+	{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+for (let i = 0; i < alumns.length; i++) {
+    if (alumns[i].T1 & alumns[i].T2 === true){
+        console.log(alumns[i].name + " isApproved");
+    } else if(alumns[i].T1 & alumns[i].T3 === true){
+        console.log(alumns[i].name + " isApproved");
+    } else if(alumns[i].T2 & alumns[i].T3 === true){
+        console.log(alumns[i].name + " isApproved");
+    }
+    
+}
